0c17df0cb7a20e0103e3a7c3f345aebbd4b03432
AlekseyShteinke <43885149+AlekseyShteinke@users.noreply.github.com>
2021-05-05 18:54:32 +0300

    удаляет всё кроме первых 10 записей

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ VK_Wall_Problem
 
 """
 import vk_api
-from config import token  # Main token
-
-
-vk = vk_api.VkApi(token=token).get_api()
 
 
 def main():
-    pass
+
+    #Authentification
+    vk_session = vk_api.VkApi('***', '***')#login, password
+    vk_session.auth()
+    vk = vk_session.get_api()
+
+
+    #delete posts, exept first 10
+    posts = vk.wall.get(count=100, offset=10)['items']
+    while posts:
+        for post in posts:
+            print(post['id'])
+            vk.wall.delete(post_id=post['id'])
+        posts = vk.wall.get(count=100, offset=10)['items']
 
 
 if __name__ == '__main__':
